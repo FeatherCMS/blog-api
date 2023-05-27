27526600bc3909c0e6f99bb9fc818842ac307761
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2023-05-26 23:34:13 -0500

    rename/resolve deps

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
         .library(name: "BlogObjects", targets: ["BlogObjects"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/feathercms/feather-objects", .branch("main")),
+		.package(path: "../feather-objects"),
+//        .package(url: "https://github.com/feathercms/feather-objects", .branch("main")),
     ],
     targets: [
         .target(name: "BlogObjects", dependencies: [
