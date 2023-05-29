5b61ca2d8fda7a09f3387958c6930118d85a8005
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2023-05-29 08:15:50 -0500

    refactor dep urls for merge

--- Package.swift
+++ Package.swift
@@ -4,15 +4,13 @@ import PackageDescription
 let package = Package(
     name: "blog-objects",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v12)
     ],
     products: [
         .library(name: "BlogObjects", targets: ["BlogObjects"]),
     ],
     dependencies: [
-//		.package(path: "../feather-objects"),
-        .package(url: "https://github.com/Rando-Coderissian/feather-objects", .branch("test-refactor-modules")),
-//        .package(url: "https://github.com/feathercms/feather-objects", .branch("main")),
+       .package(url: "https://github.com/feathercms/feather-objects", .branch("main")),
     ],
     targets: [
         .target(name: "BlogObjects", dependencies: [
